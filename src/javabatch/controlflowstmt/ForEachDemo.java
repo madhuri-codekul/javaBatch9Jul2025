a0446a90cf975a9ae75565990ecf3edcd912a00d
Madhuri Tayade <mt202202@appystack.com>
2025-07-31 14:10:33 +0530

    break,continue,do--while

--- src/javabatch/controlflowstmt/ForEachDemo.java
+++ src/javabatch/controlflowstmt/ForEachDemo.java
@@ -3,11 +3,12 @@ package javabatch.controlflowstmt;
 public class ForEachDemo {
     public static void main(String[] args) {
         int a[] = {1,2,3};
-        String str[] = {"Hello","World","!!!"};
+
         for (int i:a){
             System.out.println(i);
         }
 
+        String str[] = {"Hello","World","!!!"};
         for (String s:str){
             System.out.print(s);
         }
